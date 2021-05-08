41b63a0e3733a0cd4fe3ed1f0ecb700e929be61f
Sopee Thong <tsopee@gmail.com>
2021-05-07 22:17:25 -0400

    adding model

---
+++ Develop/models/workout.js
@@ -0,0 +1,14 @@
+const mongoose = require ('mongoose');
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema ({
+    day: {},
+    exercises: 
+
+});
+
+const Workout = mongoose.model('Workout',
+workoutSchema);
+
+module.exports = Workout;
